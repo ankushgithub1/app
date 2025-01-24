af6dc16dee79dffbd66ac46ee59a765ee31177bd
ankushgithub1 <102030878+ankushgithub1@users.noreply.github.com>
2025-01-24 12:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Node.js App! & Welcome Ankush ");
+  res.send("Hello World from Node.js App");
 });
 
 app.listen(port, () => {
