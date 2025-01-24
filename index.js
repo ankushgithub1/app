d32dbd73b42421378808512497be120a37e78836
ankushgithub1 <102030878+ankushgithub1@users.noreply.github.com>
2025-01-24 10:52:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Node.js App!");
+});
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
